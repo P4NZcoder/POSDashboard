4437af6d04834b33ca86dcfc80dbe1f16b062e0d
P4NZcoder <putanapong.sb@gmail.com>
2026-05-13 13:55:20 +0700

    Change base path in vite.config.js
    Updated base path in Vite configuration for GitHub repo.

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
-import react from "@vitejs/plugin-react";
-import { defineConfig } from "vite";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: "./",
   plugins: [react()],
-});
-
+  base: '/POSDashboard/', // เปลี่ยนตรงนี้ให้ตรงกับชื่อ Repo บน GitHub
+})
